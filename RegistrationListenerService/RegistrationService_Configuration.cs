44f3e2252daf5561078a866ca3484b2465616b26
thanosbil <bilalis.ath@gmail.com>
2021-05-02 16:33:59 +0300

    added remarks

--- RegistrationListenerService/RegistrationService_Configuration.cs
+++ RegistrationListenerService/RegistrationService_Configuration.cs
@@ -7,11 +7,35 @@ using System.Threading.Tasks;
 
 namespace RegistrationListenerService {
     public class RegistrationService_Configuration : IRegistrationService_Configuration {
+
+        /// <summary>
+        /// The endpoint for the messaging service
+        /// </summary>
         public string Endpoint { get; set; }
+
+        /// <summary>
+        /// The name of the exchange
+        /// </summary>
         public string ExchangeName{ get; set; }
+
+        /// <summary>
+        /// The name of the queue to check for messages
+        /// </summary>
         public string QueueName { get; set; }
+
+        /// <summary>
+        /// The routing key of the queue
+        /// </summary>
         public string RoutingKey { get; set; }
+
+        /// <summary>
+        /// The path where the file will be stored
+        /// </summary>
         public string FileOutputPath { get; set; }
+
+        /// <summary>
+        /// The file name
+        /// </summary>
         public string FileOutputName { get; set; }
     }
 }
